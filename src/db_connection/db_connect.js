2d48cf8e9754dd3b0ddf97e7fb6191f39b652b72
Umar Farooque <umarfarooque@Umars-MacBook-Air.local>
2024-03-23 01:18:17 +0530

    Configured env files

--- src/db_connection/db_connect.js
+++ src/db_connection/db_connect.js
@@ -1,15 +1,12 @@
-// require("dotenv").config();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connectDatabase() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://prologic313:yxmQS1QDXeVp3gga@headless-cms-db.jhgrke7.mongodb.net/?retryWrites=true&w=majority&appName=headless-cms-db&ssl=true",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
